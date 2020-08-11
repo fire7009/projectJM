5f79f32118847cae3cc5700f06f3d92546149cf7
STU <STU@DESKTOP-MDA8DDN>
2020-08-11 18:58:33 +0900

    DTO 생성

---
+++ jm_project/src/jm_dto/OrdProdHisDTO.java
@@ -0,0 +1,82 @@
+package jm_dto;
+
+public class OrdProdHisDTO {
+	private String hisSeqno;
+	private String ordNo;
+	private String prodCd;
+	private int ordQty;
+	private String frstRgsrUsrno;
+	private String frstRgstDttm;
+	private String lastProcrUsrno;
+	private String lastProcDttm;
+	
+	public OrdProdHisDTO() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getHisSeqno() {
+		return hisSeqno;
+	}
+
+	public void setHisSeqno(String hisSeqno) {
+		this.hisSeqno = hisSeqno;
+	}
+
+	public String getOrdNo() {
+		return ordNo;
+	}
+
+	public void setOrdNo(String ordNo) {
+		this.ordNo = ordNo;
+	}
+
+	public String getProdCd() {
+		return prodCd;
+	}
+
+	public void setProdCd(String prodCd) {
+		this.prodCd = prodCd;
+	}
+
+	public int getOrdQty() {
+		return ordQty;
+	}
+
+	public void setOrdQty(int ordQty) {
+		this.ordQty = ordQty;
+	}
+
+	public String getFrstRgsrUsrno() {
+		return frstRgsrUsrno;
+	}
+
+	public void setFrstRgsrUsrno(String frstRgsrUsrno) {
+		this.frstRgsrUsrno = frstRgsrUsrno;
+	}
+
+	public String getFrstRgstDttm() {
+		return frstRgstDttm;
+	}
+
+	public void setFrstRgstDttm(String frstRgstDttm) {
+		this.frstRgstDttm = frstRgstDttm;
+	}
+
+	public String getLastProcrUsrno() {
+		return lastProcrUsrno;
+	}
+
+	public void setLastProcrUsrno(String lastProcrUsrno) {
+		this.lastProcrUsrno = lastProcrUsrno;
+	}
+
+	public String getLastProcDttm() {
+		return lastProcDttm;
+	}
+
+	public void setLastProcDttm(String lastProcDttm) {
+		this.lastProcDttm = lastProcDttm;
+	}
+	
+	
+}
---
+++ jm_project/src/jm_dto/OrderInfoDTO.java
@@ -0,0 +1,172 @@
+package jm_dto;
+
+public class OrderInfoDTO {
+	private String ordNo;
+	private int ordSumQty;
+	private int ordSumAmt;
+	private String rcvrNm;
+	private String dlvrPostCd;
+	private String dlvrBasAddr;
+	private String dlvrDetlAddr;
+	private String ordPathDv;
+	private String paymtDvcd;
+	private String paystDvcd;
+	private String ordPrcstDvcd;
+	private String ordCnclReqYn;
+	private String ordCnclYn;
+	private String ordCnclRsDvcd;
+	private String frstRgsrUsno;
+	private String frstRgstDttm;
+	private String lastProcrUsrno;
+	private String lastProcDttm;
+	
+	public OrderInfoDTO() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getOrdNo() {
+		return ordNo;
+	}
+
+	public void setOrdNo(String ordNo) {
+		this.ordNo = ordNo;
+	}
+
+	public int getOrdSumQty() {
+		return ordSumQty;
+	}
+
+	public void setOrdSumQty(int ordSumQty) {
+		this.ordSumQty = ordSumQty;
+	}
+
+	public int getOrdSumAmt() {
+		return ordSumAmt;
+	}
+
+	public void setOrdSumAmt(int ordSumAmt) {
+		this.ordSumAmt = ordSumAmt;
+	}
+
+	public String getRcvrNm() {
+		return rcvrNm;
+	}
+
+	public void setRcvrNm(String rcvrNm) {
+		this.rcvrNm = rcvrNm;
+	}
+
+	public String getDlvrPostCd() {
+		return dlvrPostCd;
+	}
+
+	public void setDlvrPostCd(String dlvrPostCd) {
+		this.dlvrPostCd = dlvrPostCd;
+	}
+
+	public String getDlvrBasAddr() {
+		return dlvrBasAddr;
+	}
+
+	public void setDlvrBasAddr(String dlvrBasAddr) {
+		this.dlvrBasAddr = dlvrBasAddr;
+	}
+
+	public String getDlvrDetlAddr() {
+		return dlvrDetlAddr;
+	}
+
+	public void setDlvrDetlAddr(String dlvrDetlAddr) {
+		this.dlvrDetlAddr = dlvrDetlAddr;
+	}
+
+	public String getOrdPathDv() {
+		return ordPathDv;
+	}
+
+	public void setOrdPathDv(String ordPathDv) {
+		this.ordPathDv = ordPathDv;
+	}
+
+	public String getPaymtDvcd() {
+		return paymtDvcd;
+	}
+
+	public void setPaymtDvcd(String paymtDvcd) {
+		this.paymtDvcd = paymtDvcd;
+	}
+
+	public String getPaystDvcd() {
+		return paystDvcd;
+	}
+
+	public void setPaystDvcd(String paystDvcd) {
+		this.paystDvcd = paystDvcd;
+	}
+
+	public String getOrdPrcstDvcd() {
+		return ordPrcstDvcd;
+	}
+
+	public void setOrdPrcstDvcd(String ordPrcstDvcd) {
+		this.ordPrcstDvcd = ordPrcstDvcd;
+	}
+
+	public String getOrdCnclReqYn() {
+		return ordCnclReqYn;
+	}
+
+	public void setOrdCnclReqYn(String ordCnclReqYn) {
+		this.ordCnclReqYn = ordCnclReqYn;
+	}
+
+	public String getOrdCnclYn() {
+		return ordCnclYn;
+	}
+
+	public void setOrdCnclYn(String ordCnclYn) {
+		this.ordCnclYn = ordCnclYn;
+	}
+
+	public String getOrdCnclRsDvcd() {
+		return ordCnclRsDvcd;
+	}
+
+	public void setOrdCnclRsDvcd(String ordCnclRsDvcd) {
+		this.ordCnclRsDvcd = ordCnclRsDvcd;
+	}
+
+	public String getFrstRgsrUsno() {
+		return frstRgsrUsno;
+	}
+
+	public void setFrstRgsrUsno(String frstRgsrUsno) {
+		this.frstRgsrUsno = frstRgsrUsno;
+	}
+
+	public String getFrstRgstDttm() {
+		return frstRgstDttm;
+	}
+
+	public void setFrstRgstDttm(String frstRgstDttm) {
+		this.frstRgstDttm = frstRgstDttm;
+	}
+
+	public String getLastProcrUsrno() {
+		return lastProcrUsrno;
+	}
+
+	public void setLastProcrUsrno(String lastProcrUsrno) {
+		this.lastProcrUsrno = lastProcrUsrno;
+	}
+
+	public String getLastProcDttm() {
+		return lastProcDttm;
+	}
+
+	public void setLastProcDttm(String lastProcDttm) {
+		this.lastProcDttm = lastProcDttm;
+	}
+	
+	
+}
