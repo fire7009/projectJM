60c8d78c27de8cfe2d28d83dc029ec73bbe90c2d
REINA <REINA@DESKTOP-7DHDM7U>
2020-08-11 20:18:06 +0900

    user_info DAO

--- jm_project/src/jm_dao/UserInfoDAO.java
+++ jm_project/src/jm_dao/UserInfoDAO.java
@@ -1,9 +1,10 @@
 package jm_dao;
 
+import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
-import com.sun.corba.se.pept.transport.Connection;
 
 import jm_dto.UserInfoDTO;
 
@@ -23,6 +24,9 @@ public class UserInfoDAO extends JdbcDAO {
 		return _dao;
 	}
 	
+	
+	//회원정보 전달받아  UserInfo 테이블에 삽입하고 삽입행의 갯수를 반환하는 메소드 
+	//=> 아이디, 패스워드, 이름, 연락처, 우편번호, 기본주소, 상세주소, 회원구분 
 	public int insertUserInfo(UserInfoDTO userInfo) {
 		Connection con=null;
 		PreparedStatement pstmt=null;
@@ -30,41 +34,115 @@ public class UserInfoDAO extends JdbcDAO {
 		try {
 	         con=getConnection();
 	         
-	         String sql="insert into userInfo values(?,?,?,?,?,?,?,?,sysdate,null,1)";
+	         String sql="insert into userInfo(user_id, password, user_nm, cont_addr, post_cd, bas_addr, "
+	         		+ "detl_addr, user_dv) values(?,?,?,?,?,?,?,?)";
 	         pstmt=con.prepareStatement(sql);
-	         pstmt.setString(1, userInfo.getUserNo());
-	         pstmt.setString(2, userInfo.getUserId());
-	         pstmt.setString(3, userInfo.getPassword());
-	         pstmt.setString(4, userInfo.getUserNm());
-	         pstmt.setString(5, userInfo.getContAddr());
-	         pstmt.setString(6, userInfo.getPostCd());
-	         pstmt.setString(7, userInfo.getBasAddr());
-	         pstmt.setString(8, userInfo.getDetlAddr());
+	         pstmt.setString(1, userInfo.getUserId());
+	         pstmt.setString(2, userInfo.getPassword());
+	         pstmt.setString(3, userInfo.getUserNm());
+	         pstmt.setString(4, userInfo.getContAddr());
+	         pstmt.setString(5, userInfo.getPostCd());
+	         pstmt.setString(6, userInfo.getBasAddr());
+	         pstmt.setString(7, userInfo.getDetlAddr());
+	         pstmt.setString(8, userInfo.getUserDv());
 	         
 	         rows=pstmt.executeUpdate();
 	      } catch (SQLException e) {
-	         System.out.println("[에러]insertMember() 메소드의 SQL 오류 = "+e.getMessage());
+	         System.out.println("[에러]insertUserInfo() 메소드의 SQL 오류 = "+e.getMessage());
+	      } finally {
+	         close(con, pstmt);
+	      }
+	      return rows;
+	   }
+	
+	
+	
+	//회원정보 상세 조회 
+	//회원번호를 전달받아 user_info 테이블에 저장된 해당 아이디의 회원 정보를 검색하여 반환하는 메소드
+	 public UserInfoDTO selectUserInfo(String userNo) {
+	      Connection con=null;
+	      PreparedStatement pstmt=null;
+	      ResultSet rs=null;
+	      UserInfoDTO userInfo=null;
+	      try {
+	         con=getConnection();
+	         
+	         String sql="select user_no, user_id, user_nm, cont_addr, post_cd, bas_addr, detl_addr, to_char(first_rgst_dttm,"
+	         		+ "'yyyy-mm-dd') from user_info where user_no=? ";
+	         pstmt=con.prepareStatement(sql);
+	         pstmt.setString(1, userNo);
+	         
+	         rs=pstmt.executeQuery();
+	         
+	         if(rs.next()) {
+	        	userInfo=new UserInfoDTO();
+	        	userInfo.setUserNo(rs.getString("userNo"));
+	        	userInfo.setUserId(rs.getString("userId"));
+	            //userInfo.setPassword(rs.getString("password"));     비밀번호는 필요없으려나????
+	        	userInfo.setUserNm(rs.getString("userNm"));
+	        	userInfo.setContAddr(rs.getString("contAddr"));
+	        	userInfo.setPostCd(rs.getString("postCd"));
+	        	userInfo.setBasAddr(rs.getString("basAddr"));
+	        	userInfo.setDetlAddr(rs.getString("detlAddr"));
+	        	userInfo.setUserDv(rs.getString("firstRgstDttm"));
+	            
+	         }
+	      } catch (SQLException e) {
+	         System.out.println("[에러]selectUserInfo() 메소드의 SQL 오류 = "+e.getMessage());
+	      } finally {
+	         close(con, pstmt, rs);
+	      }
+	      return userInfo;
+	   }
+
+	 	
+
+	   //회원정보를 전달받아 MEMBER 테이블에 저장된 회원정보를 변경하고 변경행의 갯수를 반환하는 메소드
+	   public int updateUserInfo(UserInfoDTO userInfo) {
+	      Connection con=null;
+	      PreparedStatement pstmt=null;
+	      int rows=0;
+	      try {
+	         con=getConnection();
+	         
+	         String sql="update user_info set user_nm=?, cont_addr=?, post_cd=?"
+	         		+ "bas_addr=?, detl_addr=?, last_procr_usrno=?, last_proc_dttm=sysdate where user_no=?";
+	         pstmt=con.prepareStatement(sql);
+	         pstmt.setString(1, userInfo.getUserNm());
+	         pstmt.setString(2, userInfo.getContAddr());
+	         pstmt.setString(3, userInfo.getPostCd());
+	         pstmt.setString(4, userInfo.getBasAddr());
+	         pstmt.setString(5, userInfo.getDetlAddr());
+	         pstmt.setString(6, userInfo.getLastProcrUsrno());
+	         pstmt.setString(7, userInfo.getLastProcDttm());
+	         
+	         rows=pstmt.executeUpdate();
+	      } catch (SQLException e) {
+	         System.out.println("[에러]updateUserInfo() 메소드의 SQL 오류 = "+e.getMessage());
 	      } finally {
 	         close(con, pstmt);
 	      }
 	      return rows;
 	   }
 
-	/*
-	String userNo;
-	private String userId;o
-	private String password;o
-	private String userNm;o
-	private String contAddr;o
-	private String postCd;o
-	private String basAddr;o
-	private String detlAddr;o
+	 
+	 
+/*	private String userNo;
+	private String userId;
+	private String password;
+	private String userNm;
+	private String contAddr;
+	private String postCd;
+	private String basAddr;
+	private String detlAddr;
 	private String userDv;
 	private String duplJoinYn;
+	private String withdYn;
 	private String firstRgsrUsrno;
 	private String firstRgstDttm;
 	private String lastProcrUsrno;
 	private String lastProcDttm;
+	
 	*/
 	
 	
