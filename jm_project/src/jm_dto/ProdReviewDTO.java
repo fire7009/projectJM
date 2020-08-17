04d8d471276cb29d050c69632649122f743bbbb0
STU <STU@DESKTOP-MDA8DDN>
2020-08-17 16:35:14 +0900

    리뷰 DTO 수정

--- jm_project/src/jm_dto/ProdReviewDTO.java
+++ jm_project/src/jm_dto/ProdReviewDTO.java
@@ -9,7 +9,11 @@ PROD_GRD         NOT NULL NUMBER(1)
 FRST_RGSR_USRNO  NOT NULL VARCHAR2(8)   
 FRST_RGST_DTTM   NOT NULL DATE          
 LAST_PROCR_USRNO NOT NULL VARCHAR2(8)   
-LAST_PROC_DTTM   NOT NULL DATE  
+LAST_PROC_DTTM   NOT NULL DATE
+READCOUNT                 NUMBER(4)     
+REF                       NUMBER(4)     
+RE_STEP                   NUMBER(4)     
+RE_LEVEL                  NUMBER(4)    
  */
 public class ProdReviewDTO {
 	private String postNo;
@@ -20,6 +24,10 @@ public class ProdReviewDTO {
 	private String frstRgstDttm;
 	private String lastProcrUsrno;
 	private String lastProcDttm;
+	private int readcount;
+	private int ref;
+	private int reStep;
+	private int reLevel;
 	
 	public ProdReviewDTO() {
 		// TODO Auto-generated constructor stub
@@ -88,6 +96,39 @@ public class ProdReviewDTO {
 	public void setLastProcDttm(String lastProcDttm) {
 		this.lastProcDttm = lastProcDttm;
 	}
+
+	public int getReadcount() {
+		return readcount;
+	}
+
+	public void setReadcount(int readcount) {
+		this.readcount = readcount;
+	}
+
+	public int getRef() {
+		return ref;
+	}
+
+	public void setRef(int ref) {
+		this.ref = ref;
+	}
+
+	public int getReStep() {
+		return reStep;
+	}
+
+	public void setReStep(int reStep) {
+		this.reStep = reStep;
+	}
+
+	public int getReLevel() {
+		return reLevel;
+	}
+
+	public void setReLevel(int reLevel) {
+		this.reLevel = reLevel;
+	}
+	
 	
 	
 }
