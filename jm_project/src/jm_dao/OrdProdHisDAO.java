7f1da1851cac6d6b978f664fc3f7e9c13d0f7d0f
STU <STU@DESKTOP-7HIO53H>
2020-08-21 16:18:21 +0900

    dd

--- jm_project/src/jm_dao/OrdProdHisDAO.java
+++ jm_project/src/jm_dao/OrdProdHisDAO.java
@@ -14,7 +14,7 @@ public class OrdProdHisDAO extends JdbcDAO {
 	private static OrdProdHisDAO _dao;
 	
 	public OrdProdHisDAO() {
-		// TODO Auto-generated constructor stubs
+		// TODO Auto-generated constructor stubsss
 	}
 	
 	static {
